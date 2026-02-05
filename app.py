9fba987bea71f138b52fe2fd6604d8e7779ca033
jerremyniehaus-lgtm <jerremy.niehaus@signetjewelers.com>
2026-02-05 16:02:09 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Promo Screenshot App", layout="wide")
+
+st.title("Promo Screenshot App")
+st.write("Deployed on Render. Next step will add screenshots + PDF.")
